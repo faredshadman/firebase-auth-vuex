b30c3fb57d503edde7ac261a77d0f07af434d544
Fərid Şadmanlı <90213680+faredshadman@users.noreply.github.com>
2022-05-08 14:25:26 +0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDm9aHGVg5ZjsrhUosPli3UvbJURQcbsWo",
-  authDomain: "vuex-4-fb-auth-67dbb.firebaseapp.com",
-  projectId: "vuex-4-fb-auth-67dbb",
-  storageBucket: "vuex-4-fb-auth-67dbb.appspot.com",
-  messagingSenderId: "569091051963",
-  appId: "1:569091051963:web:224108d76d6ca87fd8f49e",
+ your firebase config
 };
 
 //init Firebase
